f0af67f253788fd5ded4836dd2324bf206a342de
Nicolai Knudsen Vatne <nicovatne@gmail.com>
2018-09-19 19:33:43 +0200

    Added functonality to IntellJ & Ran a few tests

--- src/test/HttpTest.java
+++ src/test/HttpTest.java
@@ -1,13 +1,18 @@
+import no.kristiania.pgr200.http.HttpRequest;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+
+import java.io.IOException;
 
 public class HttpTest {
 
 	@Test
 	public void shouldExecuteRequest() throws IOException {
 		HttpRequest request = new HttpRequest("urlecho.appspot.com", 80, "/echo?status=200");
-		HttpResponse response = request.execute();
+		HttpRequest response = request.execute();
 		
-		assertThat(response.getStatusCode()).isEqualTo(200);
+		assertThat(response.getStatusCode()).isEqualTo(200);*/
 		
 	}
 }
